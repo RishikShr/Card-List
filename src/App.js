29282f28ed1fd804f722da17a61d48371b8cf602
RishikShr <48129871+RishikShr@users.noreply.github.com>
2020-08-06 02:48:29 +0530

    components created and link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component} from 'react';
 import {CardList } from './components/card-list/card-list.component'
 import './App.css';
+import {SearchBox} from './components/search-box/search-box.component'
+
 
 class App extends Component{
   constructor(){
     super();
     this.state = {
-      monsters:[]
+      monsters:[],
+      searchField:''
       
     };
   }
@@ -16,19 +19,25 @@ class App extends Component{
     .then(users => this.setState({monsters:users}))
   }
 render(){
+
+  const{ monsters , searchField } =  this.state;
+  const filteredMonsters = monsters.filter(monster => 
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+     
+
   return(  
     
      <div className="App">
-       <CardList monsters = {this.state.monsters}  />
+       
+     <SearchBox 
+     placeholder='search monsters'
+     handleChange = {e =>
+      this.setState({ searchField: e.target.value})}
+     />
+      
+       <CardList monsters = {filteredMonsters}  />
     
-
-
-
-
-
-   
-   
-
 </div>
 
 
