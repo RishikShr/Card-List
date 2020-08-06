d41e5acd4b5da15a138c09b9a73aa8e4b24ae072
RishikShr <48129871+RishikShr@users.noreply.github.com>
2020-08-07 02:57:30 +0530

    Updates

---
+++ precache-manifest.b5a1b8a760aac246d3ec3cc50ed3d290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b878d8d5e4b6f36cd9254477d7ea86f3",
+    "url": "/Card-List/index.html"
+  },
+  {
+    "revision": "988af33f5d4d47ef3b56",
+    "url": "/Card-List/static/css/main.cf8a0afa.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Card-List/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Card-List/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "988af33f5d4d47ef3b56",
+    "url": "/Card-List/static/js/main.6c36a6ce.chunk.js"
+  },
+  {
+    "revision": "05a6f1df400886163504",
+    "url": "/Card-List/static/js/runtime-main.20c725ee.js"
+  }
+]);
